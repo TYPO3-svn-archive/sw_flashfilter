23a4cfb995171e2005a61cb41f62f88ea0f10c90
sitewards <sitewards@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-23 16:21:38 +0000

    initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sw_flashfilter/trunk@61036 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "sw_flashfilter".
+#
+# Auto generated 03-04-2012 17:52
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FlashFilter',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Michael Ryvlin',
+	'author_email' => 'michael.ryvlin@sitewards.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"53a5";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b624";s:14:"ext_tables.php";s:4:"b5d6";s:14:"ext_tables.sql";s:4:"3814";s:24:"ext_typoscript_setup.txt";s:4:"e850";s:37:"icon_tx_swflashfilter_flashblocks.gif";s:4:"475a";s:13:"locallang.xml";s:4:"418b";s:16:"locallang_db.xml";s:4:"8cea";s:7:"tca.php";s:4:"afd3";s:19:"doc/wizard_form.dat";s:4:"3690";s:20:"doc/wizard_form.html";s:4:"64b6";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:34:"pi1/class.tx_swflashfilter_pi1.php";s:4:"c91f";s:42:"pi1/class.tx_swflashfilter_pi1_wizicon.php";s:4:"ee51";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"1f05";}',
+);
+
+?>
